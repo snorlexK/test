7804a157dcac656f7aea5d06689ac52f90bb5753
Hyeonjae Kim <now97kim@naver.com>
2019-08-22 14:15:37 +0900

    init import

---
+++ userTable.sql
@@ -0,0 +1,24 @@
+drop table user;
+
+create table user(
+	userId varchar(20) not null primary key,
+	userPwd varchar(20) not null,
+	userPwdQ varchar(100) not null,
+	userPwdA varchar(50) not null,
+	userName varchar(30) not null,
+	userEmail varchar(50) not null,
+	userPhone1 char(3),
+	userPhone2 char(4),
+	userPhone3 char(4),
+	userSMS char(1),
+	userAddr varchar(50),
+	userBirthYear int,
+	userBirthMonth int,
+	userBirthDay int
+);
+
+desc user;
+
+select * from user;
+
+show variables like 'c%';
